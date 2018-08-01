e9e169353d27791868482251c93af7123d41a653
cassiomolin <cassio.molin@gmail.com>
2018-08-01 01:58:38 +0100

    Add user to checklist

--- src/main/java/com/cassiomolin/listify/checklist/service/ChecklistService.java
+++ src/main/java/com/cassiomolin/listify/checklist/service/ChecklistService.java
@@ -3,18 +3,34 @@ package com.cassiomolin.listify.checklist.service;
 import com.cassiomolin.listify.checklist.domain.Checklist;
 import com.cassiomolin.listify.checklist.repository.ChecklistRepository;
 import com.cassiomolin.listify.user.domain.User;
+import com.cassiomolin.listify.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.security.Principal;
 import java.util.Optional;
 
 @Service
 public class ChecklistService {
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     private ChecklistRepository checklistRepository;
 
     public Checklist createChecklist(Checklist checklist) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userRepository.findByEmail(authentication.getName()).orElseThrow(() -> new RuntimeException("User not found")); // Fix
+
+        checklist.setOwner(user);
         return checklistRepository.insert(checklist);
     }
 
