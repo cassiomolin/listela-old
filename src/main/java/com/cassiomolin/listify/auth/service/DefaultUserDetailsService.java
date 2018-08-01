8a54b5c11cf18300b23a9378925a80937bde88a7
cassiomolin <cassio.molin@gmail.com>
2018-08-01 02:05:59 +0100

    Optimize imports

--- src/main/java/com/cassiomolin/listify/auth/service/DefaultUserDetailsService.java
+++ src/main/java/com/cassiomolin/listify/auth/service/DefaultUserDetailsService.java
@@ -4,16 +4,12 @@ import com.cassiomolin.listify.auth.AuthenticatedUserDetails;
 import com.cassiomolin.listify.user.domain.User;
 import com.cassiomolin.listify.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class DefaultUserDetailsService implements UserDetailsService {
