c748720505f64336023dbddc056add1616321e42
cassiomolin <cassio.molin@gmail.com>
2018-08-08 00:21:49 +0100

    Improve constraint violation error handling

--- src/main/java/com/cassiomolin/listela/common/MethodArgumentNotValidExceptionHandler.java
+++ src/main/java/com/cassiomolin/listela/common/MethodArgumentNotValidExceptionHandler.java
@@ -1,28 +1,21 @@
 package com.cassiomolin.listela.common;
 
 import com.cassiomolin.listela.common.ApiValidationErrorDetails.ValidationError;
-import com.fasterxml.jackson.databind.BeanDescription;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.introspect.BeanPropertyDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.bind.validation.ValidationErrors;
-import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
 @ControllerAdvice
@@ -46,35 +39,40 @@ public class MethodArgumentNotValidExceptionHandler {
 
     private ResponseEntity<? extends ApiErrorDetails> handleUnprocessableEntity(MethodArgumentNotValidException exception) {
 
+        JavaType type = mapper.getTypeFactory().constructType(exception.getParameter().getParameterType());
+        List<BeanPropertyDefinition> properties = mapper.getSerializationConfig().introspect(type).findProperties();
+
+        List<ValidationError> validationErrors = exception
+                .getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> toValidationError(fieldError, properties))
+                .collect(Collectors.toList());
+
         ApiValidationErrorDetails errorDetails = new ApiValidationErrorDetails();
         errorDetails.setStatus(UNPROCESSABLE_ENTITY.value());
         errorDetails.setTitle(UNPROCESSABLE_ENTITY.getReasonPhrase());
         errorDetails.setMessage("Request cannot be processed due to validation errors");
         errorDetails.setPath(null); // FIXME
-
-        JavaType type = mapper.getTypeFactory().constructType(exception.getParameter().getParameterType());
-        List<BeanPropertyDefinition> properties = mapper.getSerializationConfig().introspect(type).findProperties();
-
-        List<ValidationError> validationErrors = exception.getBindingResult().getFieldErrors().stream().map(fieldError -> {
-
-            ValidationError validationError = new ValidationError();
-            validationError.setMessage(fieldError.getDefaultMessage());
-            validationError.setValue(fieldError.getRejectedValue());
-
-            String path = properties.stream()
-                    .filter(property -> property.getField().getName().equals(fieldError.getField()))
-                    .map(BeanPropertyDefinition::getName)
-                    .findFirst()
-                    .orElse(fieldError.getField());
-            validationError.setPath(path);
-
-            return validationError;
-
-        }).collect(Collectors.toList());
-
         errorDetails.setValidationErrors(validationErrors);
 
-
         return ResponseEntity.unprocessableEntity().body(errorDetails);
+    }
+
+    private ValidationError toValidationError(FieldError fieldError, List<BeanPropertyDefinition> properties) {
+
+        ValidationError validationError = new ValidationError();
+        validationError.setMessage(fieldError.getDefaultMessage());
+        validationError.setValue(fieldError.getRejectedValue());
+        validationError.setPath(getJsonPropertyName(fieldError, properties).orElse(fieldError.getField()));
+
+        return validationError;
+    }
+
+    private Optional<String> getJsonPropertyName(FieldError fieldError, List<BeanPropertyDefinition> properties) {
+        return properties.stream()
+                    .filter(property -> property.getField().getName().equals(fieldError.getField()))
+                    .map(BeanPropertyDefinition::getName)
+                    .findFirst();
     }
 }
