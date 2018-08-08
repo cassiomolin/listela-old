e62b9102b09cfb26d7f34c8d5fdfbce68b473a04
cassiomolin <cassio.molin@gmail.com>
2018-08-08 19:19:51 +0100

    Remove parent project and set Java version to 10

--- src/main/java/com/cassiomolin/listela/checklist/controller/model/UpdateChecklistItemDetails.java
+++ src/main/java/com/cassiomolin/listela/checklist/controller/model/UpdateChecklistItemDetails.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
