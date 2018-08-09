aace613b3f1daa734a4f142e22af6df7d8fec548
cassiomolin <cassio.molin@gmail.com>
2018-08-10 00:13:04 +0100

    Add test

--- src/test/java/com/cassiomolin/listela/user/UserTest.java
+++ src/test/java/com/cassiomolin/listela/user/UserTest.java
@@ -1,19 +1,24 @@
 package com.cassiomolin.listela.user;
 
 import com.cassiomolin.listela.AbstractTest;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
 import io.restassured.http.ContentType;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashMap;
 
 import static io.restassured.RestAssured.given;
-/import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.isEmptyString;
+import static org.hamcrest.Matchers.*;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 
@@ -24,6 +29,15 @@ public class UserTest extends AbstractTest {
     @LocalServerPort
     private int port;
 
+    @Value("${auth.jwt.secret}")
+    private String key;
+
+    @Value("${auth.jwt.issuer}")
+    private String issuer;
+
+    @Value("${auth.jwt.audience}")
+    private String audience;
+
     @Before
     public void before() {
         cleanDatabase();
@@ -32,7 +46,7 @@ public class UserTest extends AbstractTest {
     @Test
     public void shouldRegisterUser() {
 
-        var userDetails = new HashMap<String, Object>();
+        var userDetails = new HashMap<String, String>();
         userDetails.put("firstName", "Jane");
         userDetails.put("lastName", "Doe");
         userDetails.put("email", "jane.doe@mail.com");
@@ -42,12 +56,62 @@ public class UserTest extends AbstractTest {
                 .port(port)
                 .contentType(ContentType.JSON)
                 .body(userDetails)
-
-                .when()
+        .when()
                 .post("/users")
-
-                .then()
+        .then()
                 .statusCode(201)
-                .body(isEmptyString());
+                .body(isEmptyString())
+                .header(HttpHeaders.LOCATION, is(not(nullValue())));
+    }
+
+    @Test
+    public void shouldRegisterUserAndAuthenticate() {
+
+        shouldRegisterUser();
+
+        var authDetails = new HashMap<String, String>();
+        authDetails.put("email", "jane.doe@mail.com");
+        authDetails.put("password", "password");
+
+        String token =
+        given()
+                .port(port)
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .body(authDetails)
+        .when()
+                .post("/auth")
+        .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("token", is(not(nullValue())))
+                .extract().path("token");
+
+        Claims claims = parseToken(token).getBody();
+        assertNotNull(claims.getExpiration());
+        assertEquals(audience, claims.getAudience());
+        assertEquals(authDetails.get("email"), claims.getSubject());
+
+        given()
+                .port(port)
+                .accept(ContentType.JSON)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+        .when()
+                .get("/users/me")
+        .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("id", not(isEmptyOrNullString()))
+                .body("firstName", equalTo("Jane"))
+                .body("lastName", equalTo("Doe"))
+                .body("email1", equalTo(authDetails.get("email")))
+                .body("password", isEmptyOrNullString());
+    }
+
+    private Jws<Claims> parseToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(key)
+                .requireAudience(audience)
+                .parseClaimsJws(token);
     }
 }
