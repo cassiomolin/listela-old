95265dbeb1b3c5f1b3d30b9d1a44c683d6669a8e
cassiomolin <cassio.molin@gmail.com>
2018-08-09 19:31:42 +0100

    Remove unnecessary initialization

--- src/main/java/com/cassiomolin/listela/checklist/domain/Checklist.java
+++ src/main/java/com/cassiomolin/listela/checklist/domain/Checklist.java
@@ -10,6 +10,7 @@ import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
